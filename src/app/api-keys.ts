27169204d8957fff56b9e1699e084b9dfecb7d15
Ernest <ernest.bruno94@yahoo.com>
2018-04-02 16:13:28 -0700

    Firebase practice

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyAU1nfVxEK9uoLdnSh89O6wmmNFDzlltss",
+  authDomain: "epicodus-tracks.firebaseapp.com",
+  databaseURL: "https://epicodus-tracks.firebaseio.com",
+  projectId: "epicodus-tracks",
+  storageBucket: "",
+  messagingSenderId: "61226298648"
+};
